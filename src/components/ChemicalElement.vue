ebb6892c6e210eb1e7162db3dd0e7ec7c078544e
Heikki Kainulainen <hmkainul@gmail.com>
2018-12-12 21:47:18 +0100

    Add chemical element component

---
+++ src/components/ChemicalElement.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="element" v-bind:id="symbol">
+    <span class="number">{{ number }}<br></span>
+    <span class="symbol">{{ symbol }}<br></span>
+    <span class="name">{{ name }}<br></span>
+    <span class="weight">{{ weight }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private number!: string;
+  @Prop() private symbol!: string;
+  @Prop() private name!: string;
+  @Prop() private weight!: string;
+}
+</script>
+
+<style scoped lang="stylus">
+.element
+  text-align center
+  background-color #DA70D6
+  border 1px solid
+  padding-top 10px
+  padding-bottom 10px
+  padding-left 1px
+  padding-right 1px
+  width 5.55%
+  &:hover
+    cursor pointer
+  &:active
+    color: gray
+
+.name
+  font-size 0.7em
+
+.weight
+  font-size 0.6em
+
+.symbol
+  font-size 2.0em
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <ChemicalElement number="1" symbol="H" name="Hydrogen" weight="1.008"/>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
@@ -8,10 +9,12 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import ChemicalElement from '@/components/ChemicalElement.vue';
 
 @Component({
   components: {
     HelloWorld,
+    ChemicalElement,
   },
 })
 export default class Home extends Vue {}
