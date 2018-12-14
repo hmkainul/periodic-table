9e501e9be3e7a56e6b0d91bb67f3830a763d0ff5
Heikki Kainulainen <hmkainul@gmail.com>
2018-12-14 13:05:26 +0100

    Move HelloWorld to About page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,17 @@
 <template>
   <div class="home">
     <Cell v-bind:element="element" v-bind:key="element.symbol" v-for="element in elements"/>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import Cell from '@/components/Cell.vue';
 import ChemicalElement from '@/ChemicalElement';
 import json from '@/elements.json';
 
 @Component({
   components: {
-    HelloWorld,
     Cell,
   },
 })
