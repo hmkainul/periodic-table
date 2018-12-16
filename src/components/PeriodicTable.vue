f3636473e38f4e540e858842f17e30326c9a48c6
Heikki Kainulainen <hmkainul@gmail.com>
2018-12-16 08:22:53 +0100

    Add headers for columns

--- src/components/PeriodicTable.vue
+++ src/components/PeriodicTable.vue
@@ -1,6 +1,9 @@
 <template>
   <table class="home">
     <tr>
+      <td v-for="n in 18" v-bind:key="('group'+n)" class="group">{{ n }}</td>
+    </tr>
+    <tr>
       <Cell v-bind:element="element" v-bind:key="element.symbol" v-for="element in elements"/>
     </tr>
   </table>
@@ -30,4 +33,11 @@ table
   border-collapse collapse
   width 100%
   margin-bottom 20px
+
+.group
+  padding-top 5px
+  padding-bottom 5px
+
+.empty
+  border none
 </style>
