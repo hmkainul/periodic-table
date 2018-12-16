c31fe31c4e2d97be735545c3cb33118450e7f8c3
Heikki Kainulainen <hmkainul@gmail.com>
2018-12-16 09:10:30 +0100

    Add empty areas

--- src/components/PeriodicTable.vue
+++ src/components/PeriodicTable.vue
@@ -1,11 +1,22 @@
 <template>
   <table class="home">
     <tr>
-      <td v-for="n in 18" v-bind:key="('group'+n)" class="group">{{ n }}</td>
+      <td v-for="n in 18" v-bind:key="'group'+n" class="group">{{ n }}</td>
     </tr>
-    <tr>
-      <Cell v-bind:element="element" v-bind:key="element.symbol" v-for="element in elements"/>
-    </tr>
+    <template v-for="(element, index) in elements">
+      <tr v-bind:key="index">
+        <Cell v-bind:element="element" v-bind:key="element.symbol"/>
+        <template v-if="element.number === 1">
+          <td class="empty"></td>
+          <td class="empty" colspan="7" rowspan="3"></td>
+          <td class="empty" colspan="3" rowspan="3"></td>
+          <td class="empty" colspan="5"></td>
+        </template>
+      </tr>
+      <tr v-if="element.number === 118 || element.number === 71" v-bind:key="'emptyAfter'+element.number">
+        <td colspan="18" class="empty"></td>
+      </tr>
+    </template>
   </table>
 </template>
 
