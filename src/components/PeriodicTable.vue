95ce626f0bf5d13c8d0477e9a2a4f5734aea2b6b
Heikki Kainulainen <hmkainul@gmail.com>
2018-12-16 09:41:40 +0100

    Split periodic table to rows

--- src/components/PeriodicTable.vue
+++ src/components/PeriodicTable.vue
@@ -3,17 +3,19 @@
     <tr>
       <td v-for="n in 18" v-bind:key="'group'+n" class="group">{{ n }}</td>
     </tr>
-    <template v-for="(element, index) in elements">
+    <template v-for="(row, index) in rows">
       <tr v-bind:key="index">
-        <Cell v-bind:element="element" v-bind:key="element.symbol"/>
-        <template v-if="element.number === 1">
-          <td class="empty"></td>
-          <td class="empty" colspan="7" rowspan="3"></td>
-          <td class="empty" colspan="3" rowspan="3"></td>
-          <td class="empty" colspan="5"></td>
+        <template v-for="element in row">
+          <Cell v-bind:element="element" v-bind:key="element.symbol"/>
+          <template v-if="element.number === 1">
+            <td class="empty" v-bind:key="'emptyAfter'+element.number"></td>
+            <td class="empty" colspan="7" rowspan="3" v-bind:key="'emptyAfter'+element.number"></td>
+            <td class="empty" colspan="3" rowspan="3" v-bind:key="'emptyAfter'+element.number"></td>
+            <td class="empty" colspan="5" v-bind:key="'emptyAfter'+element.number"></td>
+          </template>
         </template>
       </tr>
-      <tr v-if="element.number === 118 || element.number === 71" v-bind:key="'emptyAfter'+element.number">
+      <tr v-if="index === 7 || index === 8" v-bind:key="'emptyAfter'+index">
         <td colspan="18" class="empty"></td>
       </tr>
     </template>
@@ -32,10 +34,19 @@ import json from '@/elements.json';
   },
 })
 export default class PeriodicTable extends Vue {
-  private elements: ChemicalElement[];
+  private rows: ChemicalElement[][];
   constructor() {
     super();
-    this.elements = json;
+    const left = new Set([1, 3, 11, 19, 37, 55, 87, 58, 90]);
+    function splitToRows(total: ChemicalElement[][], current: ChemicalElement) {
+      if (left.has(current.number)) {
+        total.push([current]);
+      } else {
+        total[total.length - 1].push(current);
+      }
+      return total;
+    }
+    this.rows = json.reduce(splitToRows, []);
   }
 }
 </script>
